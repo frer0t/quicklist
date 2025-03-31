450b0066fde825f7c2c7e0addfd5bc6ab88cb9cd
frérot <149209283+iamfrerot@users.noreply.github.com>
2025-03-31 14:44:10 +0200

    chore: add splash screen configure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,61 @@
 import { supabase } from "@/lib/supabase";
 import { ThemeProvider, useTheme } from "@/lib/ThemeContext";
+import { Asset } from "expo-asset";
 import { router, Stack, useSegments } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { Image, View } from "react-native";
 import "react-native-reanimated";
 
+SplashScreen.preventAutoHideAsync();
+
+const cacheImages = (images: (string | number)[]): Promise<any>[] => {
+  return images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+};
+
 export default function RootLayout() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        const imageAssets = cacheImages([
+          require("../assets/images/pen-paper.avif"),
+        ]);
+
+        await Promise.all([...imageAssets]);
+      } catch (e) {
+        console.warn("Error loading assets:", e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <ThemeProvider>
-      <RootLayoutNav />
+      <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+        <RootLayoutNav />
+      </View>
     </ThemeProvider>
   );
 }
